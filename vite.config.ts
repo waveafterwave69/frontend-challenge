cf556b22ade11e8fd270a06b6b99acb6ec785dca
Michael Shonov <waveafterwave3w2@gmail.com>
2026-04-09 23:10:43 +0300

    feat: use hash router

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    base: '/frontend-challenge/',
+export default defineConfig(({ mode }) => {
+    const isProd = mode === 'production'
+    return {
+        base: isProd ? '/frontend-challenge/' : '/',
+        plugins: [react()],
+    }
 })
